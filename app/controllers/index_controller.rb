3a6cdd9846f4d74cdd514b8efc3c392ff06c03a7
HJRandolph <hjrandolph@gmail.com>
2016-08-01 12:54:29 -0400

    the sniff test has begun

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,27 +1,11 @@
-require 'sinatra'
-
-
-#get '/' do
-#"hello world!"
-#end
-
-def valid_birthdate(input)
-	if input.length == 8 && input.match(/^[0-9]+[0-9]$/)
-		return true
-	else
-		return false
-	end
+get '/:birthdate' do
+  setup_index_view
 end
 
-def setup_index_view
-	
-		@message = get_message(birth_path_num)	
-		erb :index
-	
-end
-
-get '/newpage/' do
-	erb :newpage
+get '/message/:birth_path_num' do
+	birth_path_num = params[:birth_path_num].to_i
+	@message = Person.get_message(birth_path_num)
+	erb :index
 end
 
 get '/' do
@@ -30,38 +14,18 @@ end
 
 post '/' do
 	birthdate = params[:birthdate].gsub("-","")
-	if valid_birthdate(birthdate)
-		birth_path_num = get_birth_path_num(birthdate)
+	if Person.valid_birthdate(birthdate)
+		birth_path_num = Person.get_birth_path_num(birthdate)
 		redirect "/message/#{birth_path_num}"
-		@message = get_message(birth_path_num)	
-		erb :index
-	else
-		@error = "You should enter a valid birthdate in the form of mmddyyyy."
-		erb :form
-	end
-
+		else
+    @error = "Oops! You should enter a valid birthdate in the form of mmddyyyy. Try again!"
+    erb :form    
+  end
 end
 
-get '/message/:birth_path_num' do
-	birth_path_num = params[:birth_path_num].to_i
-	@message = get_message(birth_path_num)	
-		#@predicate = "<h4>Your numerology number...</h4>"
-	erb :index
+def setup_index_view
+	birthdate = params[:birthdate]
+	birth_path_num = Person.get_birth_path_num(birthdate)
+	@message = Person.get_message(birth_path_num)
+  erb :index
 end
-
-
-
-#trying to figure out how to make the message appear using the above case when rather than building an array to call on
-#get '/newpage' do	
-#	n = 1
-#	if n < 10
-#		@message = get_message(birth_path_num)
-#		erb :index
-#		n + 1
-#	end
-#end
-
-get '/' do
-	"Hello World"
-end
-
