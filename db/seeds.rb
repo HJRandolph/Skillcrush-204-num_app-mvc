46d782b7abd3a94ca0f50210ad9647503d951ec9
HJRandolph <hjrandolph@gmail.com>
2016-08-08 20:55:26 -0400

    updating modified files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 require 'faker'
 
-Person.create(first_name: Faker::Name.first_name, 
+(1..40).each do |create|
+	Person.create(first_name: Faker::Name.first_name, 
 	last_name: Faker::Name.last_name, birthdate: Faker::Date.between(80.years.ago, 18.years.ago))
+end
